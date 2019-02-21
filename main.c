84605a2c5ae22ad32305f0af4198386e5e82471f
CODURI Luca <Luca.CODURI@cpnv.ch>
2019-02-21 11:25:02 +0100

    fonction recusrive

--- main.c
+++ main.c
@@ -1,25 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-factorielle(int val_user){
-    int i;
 
-    for(i=val_user-1; i>=1; i--){
-        val_user *= i;
+int factorielle(int a){
+    if(a-1 == 0){
+        return a;
+    }else{
+        return factorielle(a-1) * a;
     }
 
-    printf("\n\n%i\n", val_user);
 }
+
 
 int main()
 {
-    int val_user;
-
-    printf("Entrez un nombre: ");
-    scanf("%i", &val_user);
-
-
-    factorielle(val_user);
-
-    return EXIT_SUCCESS;
+    printf("%i", factorielle(5));
 }
